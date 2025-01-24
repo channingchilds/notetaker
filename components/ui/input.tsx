1676c78d1d1760e42ddf6e06c8c14fd9e76bf01c
Channing <channingchilds11@gmail.com>
2025-01-24 13:42:43 -0700

    fix: update input and textarea types to resolve ESLint errors

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,7 +1,9 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+type InputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, "type"> & {
+  type?: React.InputHTMLAttributes<HTMLInputElement>["type"]
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
